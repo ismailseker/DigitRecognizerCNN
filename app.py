f41eb2fa47169f883076e52bfd0c79f7c37be3ab
İsmail ŞEKER <sseker665@gmail.com>
2025-01-22 04:36:22 +0300

    create model

--- app.py
+++ app.py
@@ -56,3 +56,55 @@ print("y_test shape",Y_val.shape)
 plt.imshow(X_train[2][:,:,0],cmap='gray')
 plt.axis('off')
 plt.show()
+
+from sklearn.metrics import confusion_matrix
+import itertools
+
+from tensorflow.keras.utils import to_categorical 
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPool2D, Input
+from tensorflow.keras.optimizers import RMSprop, Adam
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.callbacks import ReduceLROnPlateau
+
+
+model = Sequential()
+
+model.add(Input(shape=(28,28,1)))
+
+model.add(Conv2D(filters=8, kernel_size=(5, 5), padding='same', activation='relu'))
+model.add(MaxPool2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Conv2D(filters=16, kernel_size=(3, 3), padding='same', activation='relu'))
+model.add(MaxPool2D(pool_size=(2, 2), strides=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Flatten())
+model.add(Dense(256, activation='relu'))
+model.add(Dropout(0.5))
+
+model.add(Dense(10, activation='softmax'))
+
+model.summary()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
